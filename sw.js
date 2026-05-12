a0a4f3c894480e097eb6c5cad796037c2c8f8222
tsjwjwn <truongminhtrong500k@gmail.com>
2026-05-12 21:20:45 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "fake-gunrrun-v1";
+
+const FILES = [
+  "./",
+  "index.html",
+  "style.css",
+  "game.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  );
+});
